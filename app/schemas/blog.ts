dbfda882cafc7b7eb4e20c6943b52cdc0c95bc2b
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-12-30 19:00:19 +0530

    feat: Add image file size validation to blog schema

--- app/schemas/blog.ts
+++ app/schemas/blog.ts
@@ -6,5 +6,7 @@ export const postSchema = z.object({
     .min(3, "Title must be at least 3 characters long")
     .max(50, "Title must be at most 50 characters long"),
   content: z.string().min(10, "Content must be at least 10 characters long"),
-  image: z.instanceof(File),
+  image: z.instanceof(File).refine((file) => file.size <= 1024 * 1024, {
+    message: "File size must be less than 1MB",
+  }),
 });
